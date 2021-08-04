b1e383ee4b8aa08abb8a74ffc16458363b6bb112
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-08-04 17:29:54 +0900

    Change authenticateJWT middleware

--- middlewares/authenticateJWT.js
+++ middlewares/authenticateJWT.js
@@ -1,13 +1,49 @@
-const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const { loginUser } = require('../utils/setLoginUser');
 
-exports.authenticateJWT = (req, res, next) =>
-  passport.authenticate('jwt', { sessions: false }, (error, user, message) => {
-    if (user) {
-      req.loginUser = user;
+exports.authenticateJWT = async (req, res, next) => {
+  const [accessToken, refreshToken] = req.headers['authorization'].split(',');
+  const iAccessToken = verifyToken(accessToken);
+  const irefreshToken = verifyToken(refreshToken);
+
+  if (
+    (typeof iAccessToken === 'string' && iAccessToken.includes('invalid')) ||
+    (typeof irefreshToken === 'string' && irefreshToken.includes('invalid'))
+  ) {
+    res.status(403).json({ ok: false, message: 'wrong token' });
+  }
+  if (iAccessToken === 'jwt expired') {
+    console.log('accessToken 만료 기간지남!!!');
+    if (irefreshToken) {
+      const info = await User.findOne({
+        attributes: ['email', 'nickname', 'img'],
+        where: { refreshToken },
+      });
+      const basicInfo = {
+        email: info.email,
+        nickname: info.nickname,
+        img: info.img,
+      };
+      const accessToken = jwt.sign(basicInfo, process.env.JWT_SECRET, {
+        expiresIn: process.env.ACCESSTOKEN_EXPIRE,
+      });
+      req.loginUser = loginUser(basicInfo, accessToken, refreshToken);
       next();
     } else {
-      const error = new Error(message);
-      error.status = 403;
-      next(error);
+      res.json({ ok: false, message: '로그인이 필요합니다.' });
     }
-  })(req, res, next);
+  } else {
+    req.loginUser = loginUser(iAccessToken, accessToken, refreshToken);
+    next();
+  }
+};
+
+function verifyToken(token) {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    console.error(error);
+    return error.message;
+  }
+}
