25b06a38eaf81e476f33dea22d85df18a4dd6e05
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-08-26 02:00:17 +0900

    Change config

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: 'root',
-    password: '1234',
-    database: 'node_health',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     timezone: '+09:00',
   },
