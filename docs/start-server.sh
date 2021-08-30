3f02c47e3a9000fe9c5ad39e72ede4ca96ffc3da
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-08-30 17:47:35 +0900

    Add docs

--- docs/start-server.sh
+++ docs/start-server.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
-
 cd /deploy/node_health
 
 npm install
 
+cd /deploy/node_health/src
+
 npx sequelize db:create
+
+cd /deploy/node_health
 
 npm install pm2 -g
 
 pm2-runtime start ecosystem.config.js --env production
-
-#chmod +x ./start-server.sh
