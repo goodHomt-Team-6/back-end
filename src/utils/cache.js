9726880032f45cd7b561900b19464095fb5e71f8
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-08-31 11:11:12 +0900

    Add set&get Cache Function

---
+++ src/utils/cache.js
@@ -0,0 +1,19 @@
+const redis = require('redis');
+
+const redistHost =
+  process.env.NODE_ENV === 'production' ? '127.0.0.1' : '13.124.152.4';
+console.log(redistHost);
+const redisClient = redis.createClient(6379, redistHost);
+const DEFAULT_EXPIRATION = 3600;
+
+exports.getOrSetCache = (key, cb) => {
+  return new Promise((resolve, reject) => {
+    redisClient.get(key, async (error, data) => {
+      if (error) return reject(error);
+      if (data != null) return resolve(JSON.parse(data));
+      const freshData = await cb();
+      redisClient.setex(key, DEFAULT_EXPIRATION, JSON.stringify(freshData));
+      resolve(freshData);
+    });
+  });
+};
